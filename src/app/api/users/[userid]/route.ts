69489f86362ce26eb730b9dfc436688a02fe0e05
emon.needglobal <emon.needglobal@gmail.com>
2024-07-13 21:24:21 +0600

    api

--- src/app/api/users/[userid]/route.ts
+++ src/app/api/users/[userid]/route.ts
@@ -1,16 +1,15 @@
 import { NextResponse } from "next/server";
 import data from "@/data.json";
 
+export async function GET(request: Request, context: any) {
+  const { params } = context;
+  console.log(params);
+  console.log(params);
 
-export async function GET(request:Request,context:any) {
+  const user = data.filter((x) => params.userid === x.id.toString());
 
-    const {params}=context;
-    console.log(params)
-
-const user=data.filter((x)=>params.userid===x.id.toString());
-    
-console.log(user)
-return NextResponse.json({
+  console.log(user);
+  return NextResponse.json({
     user,
-})
+  });
 }
